d66fdf505baf9c98b6a260c3912f389400c713a8
RafaellaOliveira <54822077+RafaellaOliveira@users.noreply.github.com>
2019-09-30 17:26:24 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace trabalhoFelipe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImagem1 = new System.Windows.Forms.Button();
+            this.btnImagem2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnImagem1
+            // 
+            this.btnImagem1.BackColor = System.Drawing.Color.Transparent;
+            this.btnImagem1.BackgroundImage = global::trabalhoFelipe.Properties.Resources.xratao;
+            this.btnImagem1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnImagem1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnImagem1.Location = new System.Drawing.Point(13, 13);
+            this.btnImagem1.Name = "btnImagem1";
+            this.btnImagem1.Size = new System.Drawing.Size(300, 300);
+            this.btnImagem1.TabIndex = 0;
+            this.btnImagem1.Text = "btnImg1";
+            this.btnImagem1.UseVisualStyleBackColor = false;
+            this.btnImagem1.Click += new System.EventHandler(this.btnImagem1_Click);
+            // 
+            // btnImagem2
+            // 
+            this.btnImagem2.AutoEllipsis = true;
+            this.btnImagem2.AutoSize = true;
+            this.btnImagem2.BackColor = System.Drawing.Color.Transparent;
+            this.btnImagem2.BackgroundImage = global::trabalhoFelipe.Properties.Resources.xratao;
+            this.btnImagem2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnImagem2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnImagem2.Location = new System.Drawing.Point(319, 12);
+            this.btnImagem2.Name = "btnImagem2";
+            this.btnImagem2.Size = new System.Drawing.Size(300, 300);
+            this.btnImagem2.TabIndex = 1;
+            this.btnImagem2.Text = "btnImg2";
+            this.btnImagem2.UseVisualStyleBackColor = false;
+            this.btnImagem2.Click += new System.EventHandler(this.btnImagem2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 326);
+            this.Controls.Add(this.btnImagem2);
+            this.Controls.Add(this.btnImagem1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImagem1;
+        private System.Windows.Forms.Button btnImagem2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trabalhoFelipe
+{
+    public partial class Form1 : Form
+    {
+        Random R1,R2;
+        int numRandon1, numRandon2;
+        int[] contadorImg1 = { 0, 0, 0, 0 };
+        //int[] contadorImg2 = { 0, 0, 0, 0 };
+        int verificaContador = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            btnImagem1.Text = "";
+            btnImagem2.Text = "";
+            R1 = new Random();
+            R2 = new Random();
+            numRandon1 = R1.Next(1, 5);
+            numRandon2 = R2.Next(1, 5);
+            while (numRandon2 == numRandon1)
+            {
+                numRandon2 = R2.Next(1, 5);
+            }
+            switch (numRandon1)
+            {
+                case 1:
+                    this.btnImagem1.BackgroundImage = Properties.Resources.xratao;
+                    break;
+                case 2:
+                    this.btnImagem1.BackgroundImage = Properties.Resources.saladafrutas;
+                    break;
+                case 3:
+                    this.btnImagem1.BackgroundImage = Properties.Resources.macarrao;
+                    break;
+                case 4:
+                    this.btnImagem1.BackgroundImage = Properties.Resources.lasanha;
+                    break;
+                default:
+                    MessageBox.Show("Algo deu errado!");
+                    break;
+            }
+
+            switch (numRandon2)
+            {
+                case 1:
+                    this.btnImagem2.BackgroundImage = Properties.Resources.xratao;
+                    break;
+                case 2:
+                    this.btnImagem2.BackgroundImage = Properties.Resources.saladafrutas;
+                    break;
+                case 3:
+                    this.btnImagem2.BackgroundImage = Properties.Resources.macarrao;
+                    break;
+                case 4:
+                    this.btnImagem2.BackgroundImage = Properties.Resources.lasanha;
+                    break;
+                default:
+                    MessageBox.Show("Algo deu errado!");
+                    break;
+            }
+        }
+
+        private void btnImagem1_Click(object sender, EventArgs e)
+        {
+            numRandon1 = R1.Next(1, 5);
+            while (numRandon1 == numRandon2)
+            {
+                numRandon1 = R1.Next(1, 5);
+            }
+            switch (numRandon1)
+            {
+                case 1:
+                    this.btnImagem1.BackgroundImage = Properties.Resources.xratao;
+                    contadorImg1[0]++;
+                    break;
+                case 2:
+                    this.btnImagem1.BackgroundImage = Properties.Resources.saladafrutas;
+                    contadorImg1[1]++;
+                    break;
+                case 3:
+                    this.btnImagem1.BackgroundImage = Properties.Resources.macarrao;
+                    contadorImg1[2]++;
+                    break;
+                case 4:
+                    this.btnImagem1.BackgroundImage = Properties.Resources.lasanha;
+                    contadorImg1[3]++;
+                    break;
+                default:
+                    MessageBox.Show("Algo deu errado!");
+                    break;
+            }
+            verificaContador++;
+            if (verificaContador == 10)
+            {
+                MessageBox.Show("Resultado\n\n" +
+                    "X-Ratão : " + contadorImg1[0] + "\n" +
+                    "Salada De Fruta : " + contadorImg1[1] + "\n" +
+                    "XMacarrão : " + contadorImg1[2] + "\n" +
+                    "Lasanha : " + contadorImg1[3] + "\n", "Sistema Informa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                verificaContador = 0;
+            }
+        }
+
+        private void btnImagem2_Click(object sender, EventArgs e)
+        {
+            numRandon2 = R2.Next(1, 5);
+            while (numRandon2 == numRandon1)
+            {
+                numRandon2 = R2.Next(1, 5);
+            }
+            switch (numRandon2)
+            {
+                case 1:
+                    this.btnImagem2.BackgroundImage = Properties.Resources.xratao;
+                    contadorImg1[0]++;
+                    break;
+                case 2:
+                    this.btnImagem2.BackgroundImage = Properties.Resources.saladafrutas;
+                    contadorImg1[1]++;
+                    break;
+                case 3:
+                    this.btnImagem2.BackgroundImage = Properties.Resources.macarrao;
+                    contadorImg1[2]++;
+                    break;
+                case 4:
+                    this.btnImagem2.BackgroundImage = Properties.Resources.lasanha;
+                    contadorImg1[3]++;
+                    break;
+                default:
+                    MessageBox.Show("Algo deu errado!");
+                    break;
+            }
+            verificaContador++;
+            if (verificaContador == 10)
+            {
+                MessageBox.Show("Resultado\n\n" +
+                    "X-Ratão : " + contadorImg1[0] + "\n" +
+                    "Salada De Fruta : " + contadorImg1[1] + "\n" +
+                    "XMacarrão : " + contadorImg1[2] + "\n" +
+                    "Lasanha : " + contadorImg1[3] + "\n","Sistema Informa",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                verificaContador = 0;
+                //For para zerar vetor contador
+                for(int i = 0; i <= 3; i++)
+                {
+                    contadorImg1[i] = 0;
+                }
+            }
+        }
+    }
+}
